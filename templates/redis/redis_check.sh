9574d689e586b72ea244a3b26bf3c1bb8ae7a4a5
Denis Bozhok <kron@ostrovok.ru>
2017-07-17 22:31:02 +0300

    +qbuf support

--- templates/redis/redis_check.sh
+++ templates/redis/redis_check.sh
@@ -10,6 +10,9 @@ data_json_tail=']}'
 data_file="$(mktemp --suffix -zabbix_sender)"
 data_json_file=$(mktemp --suffix -zabbix_sender_json)
 
+trap "rm ${data_file} ${data_json_file}" EXIT SIGINT SIGTERM
+
+
 while getopts 'Z:P:h:p:a:' arg;do
   case ${arg} in
     a)
@@ -31,6 +34,7 @@ while getopts 'Z:P:h:p:a:' arg;do
   esac
 done
 
+
 redis_host=${redis_host:-127.0.0.1}
 redis_addr=${redis_addr:-${redis_host}}
 redis_port=${redis_port:-6379}
@@ -40,8 +44,11 @@ zabbix_port=${zabbix_port:-10051}
 zbx_sender_cmd="zabbix_sender -vv -z ${zabbix_host} -p ${zabbix_port}"
 data_json=${data_json_head}
 
+
+### discover databases and collect metrics ###
 db_count=0
 comma=""
+
 while read line;do
   line=${line//$'\r'/}
   line=${line//$'\n'/}
@@ -67,12 +74,36 @@ while read line;do
     echo "\"${redis_host}\" redis[${key}] ${value}" >> ${data_file}
   fi
 done < <(${timeout_cmd} /bin/bash -c "exec 3<> /dev/tcp/${redis_addr}/${redis_port}; printf 'info\r\n' >&3; cat <&3")
+###
+
+
+### get the top of clients with qbuf > 0 ###
+redis_qbuf_top=""
+redis_qbuf_max=0
+
+while read line;do
+  redis_qbuf_top+=${line}\\n
+  _redis_qbuf_max=${line##*=}
+
+  [ ${_redis_qbuf_max} -gt ${redis_qbuf_max} ] && redis_qbuf_max=${_redis_qbuf_max}
+done < <(
+  ${timeout_cmd} /bin/bash -c "
+    exec 3<> /dev/tcp/${redis_addr}/${redis_port};
+    printf 'CLIENT LIST\r\n' >&3;
+    grep 'qbuf=[1-9]\+' <&3" | sed -e 's,.*\(addr=[^ ]\+\).*\(qbuf=[^ ]\+\).*,\1 \2,'
+  )
+###
+
+
+echo \"${redis_host}\" \"redis[qbuf_top]\" \"${redis_qbuf_top}\" >> ${data_file}
+echo \"${redis_host}\" redis[qbuf_max] ${redis_qbuf_max} >> ${data_file}
+
 
 data_json="${data_json}${data_json_tail}"
 echo "\"${redis_host}\" redis.discovery ${data_json}" > ${data_json_file}
 
+
 ${zbx_sender_cmd} -i ${data_json_file}
 ${zbx_sender_cmd} -i ${data_file}
 
-rm ${data_file} ${data_json_file}
 
